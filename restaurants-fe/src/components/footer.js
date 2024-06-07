2669acd06a1959b21af5721a5a5216d63e875b80
radu <radu.patulescu@gmail.com>
2024-06-07 16:35:51 +0900

    Add link to github

--- restaurants-fe/src/components/footer.js
+++ restaurants-fe/src/components/footer.js
@@ -5,7 +5,10 @@ class Header extends Component {
     render() {
       return (
         <footer class="footer mt-auto py-3 text-white">
-            <div class="container text-center">@radami</div>
+            <div class="container text-center">
+              
+            @radami - <a href="https://github.com/Radami/random_name"> Github</a>
+            </div>
         </footer>
       );
     }

cdd67c46ad077f035efd8d33810a2275ed7609ce
radu <radu.patulescu@gmail.com>
2024-06-09 07:52:32 +0900

    Refactor Footer as function component

--- restaurants-fe/src/components/footer.js
+++ restaurants-fe/src/components/footer.js
@@ -1,17 +1,11 @@
-import { Component } from "react";
-
-
-class Header extends Component {
-    render() {
-      return (
-        <footer class="footer mt-auto py-3 text-white">
-            <div class="container text-center">
-              
-            @radami - <a href="https://github.com/Radami/random_name"> Github</a>
-            </div>
-        </footer>
-      );
-    }
-  }
+function Header() {
+  return(
+    <footer class="footer mt-auto py-3 text-white">
+        <div class="container text-center">
+        @radami - <a href="https://github.com/Radami/random_name"> Github</a>
+        </div>
+    </footer>
+  );
+}
   
-  export default Header;
+export default Header;

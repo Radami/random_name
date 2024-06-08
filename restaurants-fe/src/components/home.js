1ad6f9c4b6af3584c7f66f858a677dcbeaaa9a45
radu <radu.patulescu@gmail.com>
2024-06-09 07:47:12 +0900

    Refactor Button into function component

--- restaurants-fe/src/components/home.js
+++ restaurants-fe/src/components/home.js
@@ -3,20 +3,17 @@ import { Component } from "react";
 
 import { API_URL } from "../constants";
 
-class Button extends Component {
-
-    handleClick() {
-        console.log('click')
-    }
-
-    render() {
-        return <button onClick={this.props.onClick} className="new btn btn-warning rounded-pill">{this.props.name}</button>
-    }
+function Button({value, handleClick}) {
+    return (
+        <button onClick={handleClick} className="new btn btn-warning rounded-pill">
+            {value}
+        </button>
+    );
 }
 
 class Home extends Component {
     state ={
-        name: null
+        name: null,
     };
 
     componentDidMount() {
@@ -30,15 +27,15 @@ class Home extends Component {
     render() {
         
         return (
-                <main class="container-fluid d-flex flex-column col-lg-5 flex-grow-1 justify-content-center">
-                    <div class="row flex-grow-1">
+                <main class="container-fluid d-flex flex-column col-md-6 flex-grow-1 justify-content-center">
+                    <div class="row flex-grow-1 justify-content-center">
                         <div class="col d-flex flex-column justify-content-center text-center">
                             <div class="container-md py-3 rounded-pill bg-primary-subtle">
                                 <h3>{this.state.name}</h3>
                             </div>
                             <div class="justify-content-center align-items-center m-3">
                                 <div class="text-center">
-                                    <Button name="Surprise me" onClick={this.getNewName}/>
+                                    <Button value="Surprise me" handleClick={this.getNewName}/>
                                 </div>
                             </div>
                         </div>

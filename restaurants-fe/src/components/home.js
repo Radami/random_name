8dc0a2e444f0e868e35fa030ba020e414ea41c6e
radu <radu.patulescu@gmail.com>
2024-06-09 09:04:14 +0900

    Add icon buttons with bootstrap-icons

--- restaurants-fe/src/components/home.js
+++ restaurants-fe/src/components/home.js
@@ -3,13 +3,23 @@ import React, { useEffect, useState } from 'react';
 
 import { API_URL } from "../constants";
 
-function Button({value, handleClick}) {
+function TextButton({handleClick, buttonText, buttonClass}) {
     return (
-        <button onClick={handleClick} className="new btn btn-warning rounded-pill">
-            {value}
+        <button onClick={handleClick} className={buttonClass}>
+            {buttonText}
         </button>
     );
 }
+
+function IconButton({handleClick, buttonClass, iconClass}) {
+    return (
+        <button onClick={handleClick} class={buttonClass}>
+            <i class={iconClass} ></i>
+        </button>
+    );
+}
+
+
 
 function Home () {
   
@@ -26,14 +36,19 @@ function Home () {
     return (
         <main class="container-fluid d-flex flex-column col-md-6 flex-grow-1 justify-content-center">
             <div class="row flex-grow-1 justify-content-center">
-                <div class="col d-flex flex-column justify-content-center text-center">
-                    <div class="container-md py-3 rounded-pill bg-primary-subtle">
+                <div class="col d-flex flex-column justify-content-center ">
+                    <div class="container-md py-3 rounded-pill bg-primary-subtle text-center">
                         <h3>{name}</h3>
                     </div>
-                    <div class="justify-content-center align-items-center m-3">
-                        <div class="text-center">
-                            <Button value="Surprise me" handleClick={getNewName}/>
+                    <div class="row m-3">
+                        <div class="d-flex col justify-content-end px-0">
+                            <IconButton buttonClass="btn btn-primary rounded-circle" iconClass="bi bi-skip-backward-fill" />
                         </div>
+                        <div class="d-flex col justify-content-start px-3">
+                            {/* <TextButton buttonText="Surprise me" buttonClass="btn btn-warning rounded-pill" handleClick={getNewName} />*/}
+                            <IconButton buttonClass="btn btn-warning rounded-circle" iconClass="bi bi-arrow-clockwise" handleClick={getNewName} />                       
+                        </div>
+                        
                     </div>
                 </div>
             </div>

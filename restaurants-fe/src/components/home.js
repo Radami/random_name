01f94f4569038df7ba96084c4e456a2ae2bc2775
radu <radu.patulescu@gmail.com>
2024-06-10 08:48:48 +0900

    Add history functionality

--- restaurants-fe/src/components/home.js
+++ restaurants-fe/src/components/home.js
@@ -19,30 +19,41 @@ function IconButton({handleClick, buttonClass, iconClass}) {
     );
 }
 
-
-
-function Home () {
+export default function Home () {
   
-    const [name, setName] = useState("")
+    const [history, setHistory] = useState([]);
 
     function getNewName () {
-        axios.get(API_URL).then(res => setName(res.data.generated_name))
+        axios.get(API_URL).then(res => {
+            setHistory([...history, res.data.generated_name]);
+        })
+    }
+
+    function onBack() {
+        if (history.length > 1) {
+            setHistory(history.slice(0, history.length-1));
+        }
     }
 
     useEffect(() => {
         getNewName();
     }, []);
 
+    useEffect(() => {
+        console.log(history);
+    }, [history]);
+
+
     return (
         <main class="container-fluid d-flex flex-column col-md-6 flex-grow-1 justify-content-center">
             <div class="row flex-grow-1 justify-content-center">
                 <div class="col d-flex flex-column justify-content-center ">
                     <div class="container-md py-3 rounded-pill bg-primary-subtle text-center">
-                        <h3>{name}</h3>
+                        <h3>{history[history.length-1]}</h3>
                     </div>
                     <div class="row m-3">
                         <div class="d-flex col justify-content-end px-0">
-                            <IconButton buttonClass="btn btn-primary rounded-circle" iconClass="bi bi-skip-backward-fill" />
+                            <IconButton buttonClass="btn btn-primary rounded-circle" iconClass="bi bi-skip-backward-fill" handleClick={onBack}/>
                         </div>
                         <div class="d-flex col justify-content-start px-3">
                             {/* <TextButton buttonText="Surprise me" buttonClass="btn btn-warning rounded-pill" handleClick={getNewName} />*/}
@@ -56,6 +67,3 @@ function Home () {
         </main>
     );
 }
-
-
-export default Home;

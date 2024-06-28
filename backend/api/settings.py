b00b3d500f85543d7b504ddc8bc86e6457981a24
radu <radu.patulescu@gmail.com>
2024-06-28 10:36:10 +0900

    Change database engine to postgresql

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "random_name",
+        "USER": "random_name_user",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
